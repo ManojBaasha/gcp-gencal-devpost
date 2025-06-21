eb59125b3258a469a9302ddfe23f1473e1b4575b
ManojBaasha <manoj.elango123@gmail.com>
2025-06-21 19:45:36 +0530

    made json into .env file for rails

--- shared/firestore.py
+++ shared/firestore.py
@@ -8,11 +8,23 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Path to your service account key file
-SERVICE_ACCOUNT_PATH = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+firebase_config = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
+
 
 # Initialize Firestore DB
-cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
     
